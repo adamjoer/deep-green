5941fd24ec1a64ca03a28a839fb07232a0a30249
Adam Harald Jørgensen <58829763+adamjoer@users.noreply.github.com>
2022-11-22 22:28:35 +0100

    Remove unnecessary use of clearHighlights() in Game

--- src/game.cpp
+++ src/game.cpp
@@ -145,7 +145,7 @@ void Game::inputFen() {
 
     chessBoard.parseFen(inputStd);
 
-    clearHighlights();
+    this->highlightedSquare = nullptr;
     this->guiBoard->set(this->chessBoard);
     setTurn(chessBoard.turnToMove());
 }
@@ -165,8 +165,9 @@ void Game::outputFen() {
 void Game::reset() {
     this->chessBoard.reset();
 
-    clearHighlights();
     this->guiBoard->set(this->chessBoard);
+
+    this->highlightedSquare = nullptr;
     setTurn(chessBoard.turnToMove());
 
     statusBar()->showMessage("Game reset", 2000);
