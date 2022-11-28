8981035bac1131a78cf7deefe57c0362c4e04553
Azeliah <s205460@student.dtu.dk>
2022-11-28 12:55:25 +0100

    AI can play as black 2/2

--- src/ai/brain.cpp
+++ src/ai/brain.cpp
@@ -38,13 +38,26 @@ namespace Ai {
 
         // Child nodes = generateMoves(node)
         auto moves = node.chessBoard.pseudoLegalMoves();
+
+        if (node.move) {
+            std::sort(moves.begin(), moves.end(), [&](Chess::Move a, Chess::Move b) {
+                if (a.to == node.move->to) {
+                    if (b.to != node.move->to)
+                        return true;
+                    // else compare in a different way
+
+                }
+                return false;
+            });
+        }
+
         for (auto move: moves) {
             // Copy board
             // Perform move
             // Create node
             // Add to children vector
 
-            Chess::Board chessBoard(node.chessBoard);
+            Chess::Board chessBoard(node.chessBoard); // TODO: Needs constructor without copying move vector
             chessBoard.performMove(move);
             node.children.emplace_back(chessBoard, &node, move);
         }
@@ -52,7 +65,11 @@ namespace Ai {
         if (node.children.empty())
             return staticEvaluation(node.chessBoard);
 
-        // Order child nodes by move priority: Best move from parent node > capture last piece moved > other captures > center of the board
+        // Order child nodes by move priority:
+        // Best move from grandparent node - later
+        // > capture last piece moved
+        // > other captures
+        // > center of the board
 
 
         // value = INT32_MIN;
@@ -93,7 +110,7 @@ namespace Ai {
 
             if (teamOccupiedSquares.isOccupiedAt(square)) {
                 auto piece = static_cast<int>(chessBoard.pieceAt(square, color));
-                evaluation += pieceWeights[piece] + positionWeights[static_cast<int>(color)][piece][j];
+                evaluation -= pieceWeights[piece] + positionWeights[static_cast<int>(color)][piece][j];
 
 /*
                 std::cout << "White " << Chess::PieceType(piece) << '(' << square << "): pieceWeight="
@@ -103,7 +120,7 @@ namespace Ai {
 
             } else if (opponentOccupiedSquares.isOccupiedAt(square)) {
                 auto piece = static_cast<int>(chessBoard.pieceAt(square, opponent));
-                evaluation -= pieceWeights[piece] + positionWeights[static_cast<int>(opponent)][piece][j];
+                evaluation += pieceWeights[piece] + positionWeights[static_cast<int>(opponent)][piece][j];
 
 /*
                 std::cout << "Black " << Chess::PieceType(piece) << '(' << square << "): pieceWeight="
@@ -115,4 +132,6 @@ namespace Ai {
 
         return evaluation;
     }
+
+
 }
